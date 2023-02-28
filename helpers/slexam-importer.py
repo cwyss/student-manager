b512365679cec4c2e4b9cd4de81f16d8a873416c
cwyss <wyss@math.uni-wuppertal.de>
2023-02-28 10:26:05 +0100

    slexam-importer.py: add "master course" mode

--- helpers/slexam-importer.py
+++ helpers/slexam-importer.py
@@ -59,38 +59,52 @@ subject_translate = {
     '(Kombi) Elektrotechnik': 'Kombi ET',
     'of Education Sonderpäd (Kombi) Mathematik': 'SoPäd Mat'
 }
+subject_translate_master = {
+    'Elektrotechnik': 'ET',
+    'Electrical Engineering': 'ET',
+    'Informationstechnologie': 'IT'
+}
 
-def parse_subject(subject_lines):
+def parse_subject(subject_lines, parse_for_master):
+    if not parse_for_master:
+        regexpr = r"Bachelor ([\w\(][\w \(\).]*).*?\((\d+)\. Fachsem"
+    else:
+        regexpr = r"Master ([\w\(][\w \(\).]*).*?\((\d+)\. Fachsem"
     result = []
     for line in subject_lines:
-        match = re.match(r"Bachelor ([\w\(][\w \(\).]*).*?\((\d+)\. Fachsem",
-                         line)
+        match = re.match(regexpr, line)
         if match:
             raw_subj = match.group(1).strip()
             try:
                 subject = subject_translate[raw_subj]
-                result.append((subject, int(match.group(2))))
+                result.append((subject, int(match.group(2)), parse_for_master))
             except KeyError:
                 pass
     return result
 
-def get_current_subject(subject_entries):
-    if subject_entries:
-        current = 0
-        for (i,se) in enumerate(subject_entries):
-            if i==0:
-                pass
-            elif subject_entries[current][1] > se[1]:
-                current = i 
-        return subject_entries[current]
+def get_current_subject(subject_entries, parse_for_master):
+    current = None
+    for se in subject_entries:
+        if not current or se[1] < current[1]:
+            current = se
+    if current:
+        if not parse_for_master or current[2] == True:
+            return current[0:2]
+        else:
+            return ("Ba "+current[0], current[1])
     else:
         return None
 
 Unknown_subjects = {}
 
-def process_subjects(regist):
+def process_subjects(regist, parse_for_master):
     for e in regist:
-        current = get_current_subject(parse_subject(e['subject_long']))
+        subject_entries = parse_subject(e['subject_long'], parse_for_master=False)
+        if parse_for_master:
+            subject_entries.extend(
+                parse_subject(e['subject_long'], parse_for_master=True)
+                )
+        current = get_current_subject(subject_entries, parse_for_master)
         if current:
             (subj,sem) = current
             e['subject'] = subj
@@ -113,13 +127,13 @@ def separate_by_subject(regist_lst):
     return regist_dict
 
 
-def prepare_exam_data(examfile_list):
+def prepare_exam_data(examfile_list, parse_for_master):
     regist = []
     for sl_exam_file in examfile_list:
         regist.extend(read_slregist_csv(sl_exam_file))
     separate_first_name(regist)
     # translate_status(regist)
-    process_subjects(regist)
+    process_subjects(regist, parse_for_master)
     regist_dict = separate_by_subject(regist)
     return regist_dict
 
@@ -154,6 +168,7 @@ def print_unknown_subjects():
 
 DEBUG = False
 HELP = False
+IS_MASTER = False
 files = []
 
 for arg in sys.argv[1:]:
@@ -161,6 +176,8 @@ for arg in sys.argv[1:]:
         DEBUG = True
     elif arg=='-h':
         HELP = True
+    elif arg=='-m':
+        IS_MASTER = True
     else:
         files.append(arg)
 
@@ -174,12 +191,13 @@ REGISTRATION_FILES: csv files with studilöwe registrations.
              "E-Mail", "Status", "Studiengänge", separator ';'
 
 Options
+  -m      registrations for "master" course, prepare subjects accordingly
   -D      debug mode: write unrecognised subjects to stderr
   -h      display this help"""
     )
     sys.exit(2)
 
-regist_dict = prepare_exam_data(files)
+regist_dict = prepare_exam_data(files, parse_for_master=IS_MASTER)
 print_exam_data(regist_dict)
 if DEBUG:
     print_unknown_subjects()
