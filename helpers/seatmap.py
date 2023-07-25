1f42455974f3e89b3a237d453b14539e08379036
cwyss <wyss@math.uni-wuppertal.de>
2023-07-25 16:00:57 +0200

    seatmap.py: support for blocks and varying row lengths

--- helpers/seatmap.py
+++ helpers/seatmap.py
@@ -3,30 +3,82 @@
 import sys
 
 
-steps = []
-for a in sys.argv[1:]:
-    steps.append(int(a))
+class ArgumentError(Exception):
+    pass
 
-if len(steps)==3:
-    len_room = steps[2]
-    len_row = steps[1]
-    skip = steps[0]
 
-    len_room += 1
+class stepsizes:
+    def __init__(self, args):
+        if len(args)==0:
+            raise ArgumentError
+        try:
+            self.skip = int(args[0])
+            self.steps = []
+            for a in args[1:]:
+                t = a.partition(':')
+                if t[1]!=':':
+                    raise ArgumentError
+                i = int(t[0])
+                t = t[2].partition(':')
+                n = int(t[0]) + 1
+                if t[1]==':':
+                    t = t[2].partition(':')
+                    rowlength = int(t[0])
+                    if t[1]==':':
+                        delta = int(t[2])
+                    else:
+                        delta = 0
+                else:
+                    rowlength = n - i
+                    delta = 0
+                self.steps.append((i,n,rowlength,delta))
+        except ValueError:
+            raise ArgumentError
+
+    def generate(self):
+        seats = []
+        for block in self.steps:
+            seats.extend(self.gen_block(block))
+        return seats
+        
+    def gen_simple(self):
+        max_seat = stepsizes.len_room + 1
+        seats = []
+        i = 1 + self.shift
+        while i<max_seat:
+            next_row = min(i+self.len_row, max_seat)
+            while i<next_row:
+                seats.append(i)
+                i += self.skip
+            i = next_row + self.len_row
+        return seats
+
+    def gen_block(self, blockdata):
+        i,n,k,d = blockdata
+        seats = []
+        while i<n:
+            next_row = min(i+k,n)
+            while i<next_row:
+                seats.append(i)
+                i += self.skip
+            i = next_row + k + d
+            k += 2*d
+        return seats
+
+try:
+    stepsizes = stepsizes(sys.argv[1:])
+    seats = stepsizes.generate()
     
-    seats = []
-    i = 1
-    while i<len_room:
-        next_row = min(i+len_row, len_room)
-        while i<next_row:
-            seats.append(i)
-            i += skip
-        i = next_row+len_row
+    if seats:
+        print("%d seats total\n" % len(seats))
+        print(seats)
 
-    print("%d seats total\n" % len(seats))
-    print(seats)
-    
-else:
-    print("USAGE: seatmap.py stepsizes")
+except ArgumentError:
+    print("""USAGE: seatmap.py skip blocks...
+
+block: i:n[:k[:d]]
+   first seat i, last possible seat n, row length k,
+   row length increment d"""
+          )
     sys.exit(1)
 
