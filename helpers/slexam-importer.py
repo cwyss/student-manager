fff2e620001d60fab32d0dd809bdc19eafc206d0
cwyss <wyss@math.uni-wuppertal.de>
2025-09-11 17:55:38 +0200

    slexam-importer.py: support resit count
    Optionally add resit count to exam registration file, read from
    (separate) file from Studiloewe.
    
    Additional changes: new subject "SSS", ignore registrations with
    status "storniert"

--- helpers/slexam-importer.py
+++ helpers/slexam-importer.py
@@ -6,26 +6,74 @@ import csv, sys, re
 def read_slregist_csv(fname):
     csvfile = open(fname, newline='')
     reader = csv.reader(csvfile, delimiter=';')
-    regist = []
+    regist = {}
+    matrikel = None
     for row in reader:
         if len(row)==4:
             matrikel_field = row[1].split('@')[0]
             if matrikel_field.isdigit():
-                regist.append({'matrikel': int(matrikel_field),
-                               'name': row[0],
-                               # 'status': row[2],
-                               'subject_long': [row[3]]
-                })
-        elif len(row)==1 and len(regist)>0:
-            regist[-1]['subject_long'].append(row[0])
+                matrikel = int(matrikel_field)
+                entry = {'matrikel': matrikel,
+                         'name': row[0],
+                         'status': row[2],
+                         'subject_long': [row[3]]}
+                if entry['status']=='storniert':
+                    matrikel = None
+                    continue
+                if matrikel in regist:
+                    raise Exception("duplicate matrikel number", matrikel)
+                regist[matrikel] = entry
+        elif len(row)==1 and matrikel:
+            regist[matrikel]['subject_long'].append(row[0])
     return regist
 
 
 def separate_first_name(regist):
-    for e in regist:
-        parts = e['name'].split(',')
-        e['name'] = parts[0].strip()
-        e['first_name'] = parts[1].strip()
+    for entry in regist.values():
+        parts = entry['name'].split(',')
+        entry['name'] = parts[0].strip()
+        entry['first_name'] = parts[1].strip()
+
+
+def find_resit_fields(row):
+    try:
+        matrikel_field = row.index('Matrikelnummer')
+        resit_field = row.index('Versuch (V)')
+        return {
+            'matrikel': matrikel_field,
+            'resit': resit_field
+        }
+    except ValueError:
+        raise Exception("resit file: Column names not recognised") from None
+
+def read_resit_file(fname):
+    csvfile = open(fname, newline='')
+    reader = csv.reader(csvfile, delimiter=';')
+
+    fields = None
+    try:
+        while not fields:
+            row = next(reader)
+            if row[0]=='startHISsheet':
+                row = next(reader)
+                fields = find_resit_fields(row)
+    except (StopIteration, IndexError):
+        raise Exception("resit file: HIS csv table not found") from None
+
+    resits = []
+    for row in reader:
+        if row[0]=='endHISsheet':
+            break
+        resits.append([int(row[fields['matrikel']]),
+                       int(row[fields['resit']])])
+    return resits
+
+def add_resits(regist_dict, resit_file):
+    resits = read_resit_file(resit_file)
+    for (matrikel,resit_count) in resits:
+        if matrikel in regist_dict:
+            regist_dict[matrikel]['resit'] = resit_count
+
 
 # def translate_status(regist):
 #     status_dict = {
@@ -49,6 +97,7 @@ subject_translate = {
     'Informatik': 'Info',
     'Wirtschaftsing. Elektrotechnik': 'WIng',
     'Wirtschaftsingenieurwesen Elektrotechnik': 'WIng',
+    'Smart and Sustainable Systems': 'SSS',
     'applied science (Kombi) Physik': 'AS',
     'applied science (Kombi) Chemie': 'AS',
     'applied science (Kombi) Informatik': 'AS',
@@ -99,44 +148,49 @@ def get_current_subject(subject_entries, parse_for_master):
 Unknown_subjects = {}
 
 def process_subjects(regist, parse_for_master):
-    for e in regist:
-        subject_entries = parse_subject(e['subject_long'], parse_for_master=False)
+    for entry in regist.values():
+        long_subject = entry['subject_long']
+        subject_entries = parse_subject(long_subject, parse_for_master=False)
         if parse_for_master:
             subject_entries.extend(
-                parse_subject(e['subject_long'], parse_for_master=True)
+                parse_subject(long_subject, parse_for_master=True)
                 )
         current = get_current_subject(subject_entries, parse_for_master)
         if current:
             (subj,sem) = current
-            e['subject'] = subj
-            e['semester'] = sem
+            entry['subject'] = subj
+            entry['semester'] = sem
         elif DEBUG:
-            Unknown_subjects[e['matrikel']] = e['subject_long']
+            Unknown_subjects[entry['matrikel']] = entry['subject_long']
 
 
-def separate_by_subject(regist_lst):
-    regist_dict = {}
-    for reg in regist_lst:
+def separate_by_subject(regist):
+    regist_by_subject = {}
+    for reg in regist.values():
         try:
             subj = reg['subject']
         except KeyError:
             subj = 'unknown'
-        if subj in regist_dict:
-            regist_dict[subj].append(reg)
+        if subj in regist_by_subject:
+            regist_by_subject[subj].append(reg)
         else:
-            regist_dict[subj] = [reg]
-    return regist_dict
+            regist_by_subject[subj] = [reg]
+    return regist_by_subject
 
 
-def prepare_exam_data(examfile_list, parse_for_master):
-    regist = []
+def prepare_exam_data(examfile_list, resit_file, parse_for_master):
+    regist = {}
     for sl_exam_file in examfile_list:
-        regist.extend(read_slregist_csv(sl_exam_file))
+        regist |= read_slregist_csv(sl_exam_file)
+
     separate_first_name(regist)
     # translate_status(regist)
+    if resit_file:
+        add_resits(regist, resit_file)
+
     process_subjects(regist, parse_for_master)
-    regist_dict = separate_by_subject(regist)
-    return regist_dict
+    regist_by_subject = separate_by_subject(regist)
+    return regist_by_subject
 
 
 def print_exam_data(regist_dict):
@@ -155,7 +209,10 @@ def print_exam_data(regist_dict):
                 reg['name'], reg['first_name'],
                 str(reg['matrikel'])
             )
-            print(';'.join(data))
+            line = ';'.join(data)
+            if 'resit' in reg:
+                line += f";{reg['resit']}"
+            print(line)
 
 
 def print_unknown_subjects():
@@ -171,16 +228,24 @@ DEBUG = False
 HELP = False
 IS_MASTER = False
 files = []
+resit_file = None
 
+special = None
 for arg in sys.argv[1:]:
-    if arg=='-D':
-        DEBUG = True
-    elif arg=='-h':
-        HELP = True
-    elif arg=='-m':
-        IS_MASTER = True
+    if special=='resit_file':
+        resit_file = arg
+        special = None
     else:
-        files.append(arg)
+        if arg=='-D':
+            DEBUG = True
+        elif arg=='-h':
+            HELP = True
+        elif arg=='-m':
+            IS_MASTER = True
+        elif arg=='-r':
+            special = 'resit_file'
+        else:
+            files.append(arg)
 
 if not files:
     print(
@@ -188,18 +253,21 @@ if not files:
 Convert studilöwe exam registrations to student manager exam import file
 
 REGISTRATION_FILES: csv files with registrations.
-             Excel exports from studilöwe converted to csv, with fields:
-             "Name", "E-Mail", "Status", "Studiengänge", separator ';'
+             Excel exports from studilöwe ("Teilnehmerliste"), converted to
+             csv, with fields "Name", "E-Mail", "Status", "Studiengänge",
+             separator ';'
 
 Options
-  -m      registrations for "master" course, prepare subjects accordingly
-  -D      debug mode: write unrecognised subjects to stderr
-  -h      display this help"""
+  -m       registrations for "master" course, prepare subjects accordingly
+  -D       debug mode: write unrecognised subjects to stderr
+  -r file  read resit count from csv file containing fields "Matrikelnummer"
+           and "Versuch (V)" (studilöwe export of "Notenliste")
+  -h       display this help"""
     )
     sys.exit(2)
 
-regist_dict = prepare_exam_data(files, parse_for_master=IS_MASTER)
-print_exam_data(regist_dict)
+regist_by_subj = prepare_exam_data(files, resit_file, parse_for_master=IS_MASTER)
+print_exam_data(regist_by_subj)
 if DEBUG:
     print_unknown_subjects()
 
